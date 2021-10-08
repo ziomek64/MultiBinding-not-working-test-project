78dc95786a2b9c6cdb6b102e832de6da9f94911a
ziomek64 <mradymski3@gmail.com>
2021-10-08 02:47:11 +0200

    Add project files.

---
+++ Apptest/Apptest/DateTimetoString/BoolToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Apptest
+{
+    class BoolToColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool) values[0])
+            return values[1];
+
+            return values[2];
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Apptest/Apptest/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Apptest
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public bool _isAdultt = true;
+        public bool isAdultt
+        {
+            get => _isAdultt;
+            set
+            {
+                _isAdultt = value;
+                OnPropertyChanged();
+            }
+        }
+        public Color _favouriteColorr = Color.Green;
+        public Color favouriteColorr
+        {
+            get => _favouriteColorr;
+            set
+            {
+                _favouriteColorr = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        
+        public Color _leastfavouritee = Color.Red;
+        public Color leastfavouritee
+        {
+            get => _leastfavouritee;
+            set
+            {
+                _leastfavouritee = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            //MyLabel.SetBinding(Label.BackgroundColorProperty, new MultiBinding
+            //{
+            //    Bindings = new Collection<BindingBase>
+            //    {
+            //        new Binding("isAdultt"),
+            //        new Binding("favouriteColorr"),
+            //        new Binding("leastfavouritee")
+            //    },
+            //    Converter = new BoolToColorConverter()
+            //});
+            
+        }
+    }
+}
